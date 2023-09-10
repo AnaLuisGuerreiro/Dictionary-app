c19e0d6d4dee09fad341b3a244d504b85d6a896a
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-09-10 23:17:43 +0100

    Dictionary component (search bar)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import "./styles/App.css";
 import Dictionary from "./components/dictionary";
+
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Dictionary />
+        <header>
+          <Dictionary />
+        </header>
       </div>
     </div>
   );
--- src/components/dictionary.js
+++ src/components/dictionary.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import book from "../img/book.png";
+
+import "../styles/dictionary.css";
 
 export default function Dictionary() {
+  const [word, setWord] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(word);
+  }
+
+  function handleUserWordChange(event) {
+    setWord(event.target.value);
+  }
+
   return (
     <div className="Dictionary">
-      <form>
-        <input type="text" placeholder="Type a word" />
+      <h1> Dictionary </h1>
+      <img src={book} alt="book" width={200} />
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Type a word"
+          autoFocus={true}
+          onChange={handleUserWordChange}
+        />
         <input type="submit" value="Search" />
       </form>
     </div>
