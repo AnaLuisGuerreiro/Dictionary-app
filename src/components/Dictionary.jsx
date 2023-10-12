945efd6caf315c598778e4a55e16b30af30a2f7c
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-10-12 23:18:00 +0100

    Api request
    Passed data to results component

---
+++ src/components/Dictionary.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import Results from "./Results";
+
+import "../styles/dictionary.css";
+
+export default function Dictionary() {
+  const [word, setWord] = useState("");
+  const [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    setResults(response.data);
+  }
+
+  // API RESQUEST
+  function search(event) {
+    event.preventDefault();
+
+    // Documentation - https://www.shecodes.io/learn/apis/dictionary
+    const key_api = "37ao80323cfe0b171ed40af823227b0t";
+    const api_url = `https:api.shecodes.io/dictionary/v1/define?word=${word}&key=${key_api}`;
+
+    axios.get(api_url).then(handleResponse);
+  }
+
+  function handleUserWordChange(event) {
+    setWord(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Type a word"
+          autoFocus={true}
+          onChange={handleUserWordChange}
+        />
+        <input type="submit" value="🔍" />
+      </form>
+      <Results results={results} />
+    </div>
+  );
+}
