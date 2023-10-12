44290873a43773999a1b45152439080ce5a17887
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-10-12 21:18:17 +0100

    Api request

--- src/components/dictionary.js
+++ src/components/dictionary.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "../styles/dictionary.css";
 
 export default function Dictionary() {
   const [word, setWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(word);
+    const key_api = "37ao80323cfe0b171ed40af823227b0t";
+    const api_url = `https:api.shecodes.io/dictionary/v1/define?word=${word}&key=${key_api}`;
+
+    axios.get(api_url).then(handleResponse);
   }
 
   function handleUserWordChange(event) {
