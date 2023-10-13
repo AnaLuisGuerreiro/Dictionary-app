e51f79796252f85409085d8e058e39b19dd04c81
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-10-13 11:23:09 +0100

    Display example when have info

--- src/components/Meaning.jsx
+++ src/components/Meaning.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
+import Synonyms from "./Synonyms";
+
 export default function Meaning(props) {
-  console.log(props.meaning);
   let definition = props.meaning.definition;
   return (
     <div className="Meaning">
       <br />
       <h3>{props.meaning.partOfSpeech}</h3>
+      <strong>Definition:</strong>
       <p>"{definition[0].toUpperCase() + definition.slice(1)}"</p>
-      <em>{props.meaning.example}</em>
+
+      {/* Display examples if exist info */}
+      {props.meaning.example && (
+        <div>
+          <strong>Example:</strong>
+          <em>{props.meaning.example}</em>
+        </div>
+      )}
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
