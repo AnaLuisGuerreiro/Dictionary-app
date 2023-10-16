c23f9c339dd77a7cd9d7fd34a53c2dde9fb2edb3
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-10-16 21:39:16 +0100

    Errors message and 1 word already loaded

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -5,39 +5,65 @@ import Results from "./Results";
 
 import "../styles/dictionary.css";
 
-export default function Dictionary() {
-  const [word, setWord] = useState("");
+export default function Dictionary(props) {
+  const [word, setWord] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState(null);
 
   // API RESQUEST
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     // Documentation - https://www.shecodes.io/learn/apis/dictionary
     const key_api = "37ao80323cfe0b171ed40af823227b0t";
     const api_url = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${key_api}`;
 
-    axios.get(api_url).then((response) => {
-      setResults(response.data);
-    });
+    axios
+      .get(api_url)
+      .then((response) => {
+        if (response.data.word) {
+          setResults(response.data);
+          setError(null);
+        } else {
+          setError("Ups! English only or word not found...");
+          setResults(null);
+        }
+      })
+      .catch((error) => {
+        setError("Error finding the word!");
+        setResults(null);
+      });
   }
-
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
   function handleUserWordChange(event) {
     setWord(event.target.value);
   }
+  function load() {
+    setLoaded(true);
+    search();
+  }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          placeholder="Type a word"
-          autoFocus={true}
-          onChange={handleUserWordChange}
-        />
-        <input type="submit" value="🔍" />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form onSubmit={handleSubmit} className="mb-5">
+          <input
+            type="search"
+            placeholder="Type a word"
+            autoFocus={true}
+            onChange={handleUserWordChange}
+            className="rounded-pill "
+          />
+          <input type="submit" value="🔍" className="rounded search" />
+        </form>
+        {error ? <div className="error-message">{error}</div> : null}
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return null;
+  }
 }
