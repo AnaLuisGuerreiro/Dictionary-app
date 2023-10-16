f02f7532988b1e663c94ec7512cea0bd210de5f2
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-10-16 21:39:55 +0100

    First letter upper case

--- src/components/Meaning.jsx
+++ src/components/Meaning.jsx
@@ -3,22 +3,26 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  let definition = props.meaning.definition;
+  function capsLetter(definition) {
+    return definition.charAt(0).toUpperCase() + definition.slice(1);
+  }
+
   return (
     <div className="Meaning">
-      <br />
-      <h3>{props.meaning.partOfSpeech}</h3>
-      <strong>Definition:</strong>
-      <p>"{definition[0].toUpperCase() + definition.slice(1)}"</p>
+      <div className="shadow p-3 mb-5 bg-body-tertiary rounded mt-3 border">
+        <h3>{props.meaning.partOfSpeech}</h3>
+        <h4>Definition:</h4>
+        <p>{capsLetter(props.meaning.definition)}.</p>
 
-      {/* Display examples if exist info */}
-      {props.meaning.example && (
-        <div>
-          <strong>Example:</strong>
-          <em>{props.meaning.example}</em>
-        </div>
-      )}
-      <Synonyms synonyms={props.meaning.synonyms} />
+        {/* Display examples if exist info */}
+        {props.meaning.example && (
+          <div>
+            <h4>Example:</h4>
+            <em>" {capsLetter(props.meaning.example)}."</em>
+          </div>
+        )}
+        <Synonyms synonyms={props.meaning.synonyms} />
+      </div>
     </div>
   );
 }
