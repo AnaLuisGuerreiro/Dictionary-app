0519bee04f7bc86f4b84cd6179eef9699518bb30
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-10-16 21:37:42 +0100

    add root colors

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Styles
 import "./styles/index.css";
+import "./colors/colors.css";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
