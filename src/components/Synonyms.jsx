970c797adcc8e37216a88d026fa7c5690080256d
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-10-16 21:40:27 +0100

    First letter uper case

--- src/components/Synonyms.jsx
+++ src/components/Synonyms.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 
+import "../styles/synonyms.css";
+
 export default function Synonyms(props) {
+  function capsLetter(synonym) {
+    return synonym.charAt(0).toUpperCase() + synonym.slice(1);
+  }
+
   if (props.synonyms) {
     return (
       <div className="Synonyms">
-        <strong>Synonyms: </strong>
-        {props.synonyms.map((synonym, index) => {
-          return (
-            <ul key={index}>
-              {" "}
-              <li>{synonym}</li>{" "}
-            </ul>
-          );
-        })}
+        <div className="d-flex flex-wrap mt-5">
+          <h4 className="">Synonyms: </h4>
+          {props.synonyms.map((synonym, index) => {
+            return (
+              <ul key={index} className="mb-0">
+                {" "}
+                <li className="shadow-sm p-1 mb-5 rounded-pill">
+                  {capsLetter(synonym)}
+                </li>{" "}
+              </ul>
+            );
+          })}
+        </div>
       </div>
     );
   } else {
