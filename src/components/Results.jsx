89675d7fdb2eca5a89d6bb2d64b9d666d8b2cd1a
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-10-17 18:48:49 +0100

    long synonyms dont display

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import books from "../img/books.png";
 
 import Meaning from "./Meaning";
@@ -7,12 +6,16 @@ import Meaning from "./Meaning";
 import "../styles/results.css";
 
 export default function Results(props) {
+  function capsLetter(word) {
+    return word[0].toUpperCase() + word.slice(1);
+  }
+
   if (props.results) {
     return (
       <div className="Results">
         <div className="info-word rounded p-2 row">
           <div className="col-10">
-            <h2>{props.results.word}</h2>
+            <h2>{capsLetter(props.results.word)}</h2>
             <p>/ {props.results.phonetic} /</p>
           </div>
           <div className="col-2 text-center">
--- src/components/Synonyms.jsx
+++ src/components/Synonyms.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import "../styles/synonyms.css";
 
 export default function Synonyms(props) {
+  const hasShortSynonyms =
+    props.synonyms && props.synonyms.some((synonym) => synonym.length < 10);
+
   function capsLetter(synonym) {
     return synonym.charAt(0).toUpperCase() + synonym.slice(1);
   }
@@ -11,16 +14,21 @@ export default function Synonyms(props) {
     return (
       <div className="Synonyms">
         <div className="d-flex flex-wrap mt-5">
-          <h4 className="">Synonyms: </h4>
+          {hasShortSynonyms && <h4 className="">Synonyms: </h4>}
           {props.synonyms.map((synonym, index) => {
-            return (
-              <ul key={index} className="mb-0">
-                {" "}
-                <li className="shadow-sm p-1 mb-5 rounded-pill">
-                  {capsLetter(synonym)}
-                </li>{" "}
-              </ul>
-            );
+            // Check if synonym has less than 10 letters
+            if (synonym.length < 10) {
+              return (
+                <ul key={index} className="mb-0">
+                  {" "}
+                  <li className="shadow-sm p-1 mb-5 rounded-pill">
+                    {capsLetter(synonym)}
+                  </li>{" "}
+                </ul>
+              );
+            } else {
+              return null; // Dont display synonyms longer than 10 letters
+            }
           })}
         </div>
       </div>
